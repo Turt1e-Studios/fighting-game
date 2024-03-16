9db90470178bd6e8262fe9f35756158dc7684b41
Albert Zhan <albert.cfzhan@gmail.com>
2024-03-15 18:45:45 -0700

    Double jump

--- innocentgear/Assets/Scripts/PlayerMovement.cs
+++ innocentgear/Assets/Scripts/PlayerMovement.cs
@@ -1,24 +1,48 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float speed;
+    [SerializeField] private float runSpeed;
+    [SerializeField] private float sprintSpeed;
+    [SerializeField] private float jumpSpeed;
     
     private Vector2 _velocity;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private bool _isGrounded;
+    private bool _usedAirMove;
 
-    // Update is called once per frame
     void Update()
     {
-        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), 0);
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"),0);
         _velocity = input.normalized;
-        transform.Translate(speed * Time.deltaTime * _velocity);
+
+        transform.Translate(runSpeed * Time.deltaTime * _velocity);
+
+        if (Input.GetButtonDown("Jump") && (_isGrounded || !_usedAirMove))
+        {
+            if (!_isGrounded)
+            {
+                _usedAirMove = true;
+            }
+            GetComponent<Rigidbody2D>().AddForce(Vector3.up * jumpSpeed);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag(("Ground")))
+        {
+            _isGrounded = true;
+            _usedAirMove = false;
+        }
+    }
+ 
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag(("Ground")))
+        {
+            _isGrounded = false;
+        }
     }
 }
+
